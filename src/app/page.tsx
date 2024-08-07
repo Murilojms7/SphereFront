1fb1be7af51946836230b3791d53f967e379ab2a
murilojms <Murilojms7@gmail.com>
2024-08-07 19:59:56 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,47 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input"
 import Image from "next/image";
+import Link from "next/link"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav className="absolute right-1 top-1">
+      <nav className="absolute right-1 top-1">  
         <ModeToggle/>
       </nav>
+        <div className="p-10 flex-1 content-center mb-20 border-x-background">
 
-      <aside className="h-screen hidden lg:block">
-        <Image src="/login.png" alt="login" width={554} height={832}/>
-      </aside>
+          <aside className="pl-6">
+            <Image src="/Logo.png" alt="logo" width={100} height={100} />
+          </aside>
+          
+          <h1 className="text-5xl font-extrabold py-5 ">Bakery</h1>
 
-      <div>
-        <div className="size-[75px] bg-rose-600 rounded-full"></div>
-        <h1 className="text-4xl font-extrabold">Sphere</h1>
-        
-        <Tabs defaultValue="entrar" className="w-[400px]">
-        <TabsList>
-        <TabsTrigger value="entrar">Entrar</TabsTrigger>
-        <TabsTrigger value="registrar">Registrar</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="entrar">
-          <h1>form de login</h1>
-        </TabsContent>
-
-        <TabsContent value="registrar">
-          form de registrar
-        </TabsContent>
-        </Tabs>
-
-
-
-
-      </div>
-      
+          <Tabs defaultValue="entrar" className="w-[200px]">
+          <TabsList className="">
+          <TabsTrigger value="entrar">Entrar</TabsTrigger>
+          <TabsTrigger value="registrar">Registrar</TabsTrigger>
+          </TabsList>
+          <TabsContent value="entrar" >
+            <Input type="Login" placeholder="Login" className="mt-2"/>
+            
+            <Input type="Password" placeholder="Password" className="mt-2"/>
+            <Button asChild>
+              <Link href="/" className="mt-2">Login</Link>
+            </Button>
+          </TabsContent>
+          
+          <TabsContent value="registrar">
+          <Input type="Login" placeholder="Login" className="mt-2"/>
+            <Input type="Password" placeholder="Password" className="mt-2"/>
+            <Input type="Password" placeholder="Password Again" className="mt-2"/>
+            <Button asChild>
+              <Link href="/" className="mt-2">Registrar</Link>
+            </Button>
+          </TabsContent>
+          </Tabs>
+        </div>
     </main>
   );
 }
