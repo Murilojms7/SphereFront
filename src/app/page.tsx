d126a3e2758d9602594dc104b75bdee95a90e2f3
Murilojms7 <murilojms7@gmail.com>
2024-08-09 08:50:31 -0300

    form login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen  items-center justify-between ">
       <nav className="absolute right-1 top-1">
         <ModeToggle/>
       </nav>
@@ -22,13 +24,33 @@ export default function Home() {
         <TabsTrigger value="entrar">Entrar</TabsTrigger>
         <TabsTrigger value="registrar">Registrar</TabsTrigger>
         </TabsList>
-
         <TabsContent value="entrar">
-          <h1>form de login</h1>
+          <form action="" className="flex flex-col items-center gap-8">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="login">Login</Label>
+            <Input type="login" id="login" placeholder="login" />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password"  />
+          </div>
+          <Button>Entrar</Button>
+          </form>
         </TabsContent>
 
         <TabsContent value="registrar">
-          <h1>form de registrar</h1>
+          <form action="" className="flex flex-col items-center gap-8">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="login">Login</Label>
+            <Input type="login" id="login" placeholder="login" />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password"  />
+          </div>
+          <Button>Entrar</Button>
+          </form>
+          
         </TabsContent>
         </Tabs>
 
